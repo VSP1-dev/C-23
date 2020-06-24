3a7f047dc9fe3f7f5066123e5f41ada76a32544e
VSP1-dev <66961600+VSP1-dev@users.noreply.github.com>
2020-06-24 21:07:35 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,26 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.1,
+        'friction':2.0,
+        'density':2.3
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    angleMode(RADIANS);
+    rotate (angle);  
+    rectMode(CENTER);
+    fill(255);
+    translate (pos.x,pos.y);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
